8c53b4ef0e9302cce55757ef5d78a44df650e956
ppambler <allinlbj@gmail.com>
2021-04-07 23:26:21 +0800

    添加表单验证

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import { Form, Input, Button } from "antd";
 
 const Wrapper = styled.div`
@@ -17,16 +17,16 @@ const Title = styled.h1`
 
 const layout = {
   labelCol: {
-    span: 4,
+    span: 6,
   },
   wrapperCol: {
-    span: 20,
+    span: 18,
   },
 };
 const tailLayout = {
   wrapperCol: {
-    offset: 4,
-    span: 20,
+    offset: 6,
+    span: 18,
   },
 };
 
@@ -38,50 +38,61 @@ const Component = () => {
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+
+  // 用户名校验
+  const validateUserName = (rule, value) => {
+    if (/\W/.test(value)) return Promise.reject("只能是字母数字下划线");
+    if (value.length < 4 || value.length > 10)
+      return Promise.reject("长度为4~10个字符");
+    return Promise.resolve();
+  };
   return (
     <Wrapper>
       <Title>登录</Title>
       <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="用户名"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: '请输入你的用户名！',
-          },
-        ]}
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的用户名！",
+            },
+            { validator: validateUserName },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="密码"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: '请输入你的密码！',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的密码！",
+            },
+            { min: 4, message: "密码长度不能小于4" },
+            { max: 10, message: "密码长度不能大于10" },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          登录
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
     </Wrapper>
   );
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -17,16 +17,16 @@ const Title = styled.h1`
 
 const layout = {
   labelCol: {
-    span: 4,
+    span: 6,
   },
   wrapperCol: {
-    span: 20,
+    span: 18,
   },
 };
 const tailLayout = {
   wrapperCol: {
-    offset: 4,
-    span: 20,
+    offset: 6,
+    span: 18,
   },
 };
 
@@ -38,6 +38,23 @@ const Component = () => {
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+
+  // 用户名校验
+  const validateUserName = (rule, value) => {
+    if (/\W/.test(value)) return Promise.reject("只能是字母数字下划线");
+    if (value.length < 4 || value.length > 10)
+      return Promise.reject("长度为4~10个字符");
+    return Promise.resolve();
+  };
+
+  // 确认密码校验
+  const validateConfirm = ({ getFieldValue }) => ({
+    validator(rule, value) {
+      if (getFieldValue("password") === value) return Promise.resolve();
+      return Promise.reject("两次密码不一致");
+    },
+  });
+
   return (
     <Wrapper>
       <Title>注册</Title>
@@ -58,6 +75,9 @@ const Component = () => {
               required: true,
               message: "请输入你的用户名！",
             },
+            {
+              validator: validateUserName
+            }
           ]}
         >
           <Input />
@@ -71,6 +91,8 @@ const Component = () => {
               required: true,
               message: "请输入你的密码！",
             },
+            {min: 4, message: '密码长度不能小于4'},
+            {max: 10, message: '密码长度不能大于10'}
           ]}
         >
           <Input.Password />
@@ -79,14 +101,14 @@ const Component = () => {
         <Form.Item
           label="确认密码"
           name="confirmPassword"
-          rules={[{ required: true, message: "请输入密码！" }]}
+          rules={[{ required: true, message: "请输入密码！" },validateConfirm]}
         >
           <Input.Password />
         </Form.Item>
 
         <Form.Item {...tailLayout}>
           <Button type="primary" htmlType="submit">
-            提交
+            注册
           </Button>
         </Form.Item>
       </Form>
