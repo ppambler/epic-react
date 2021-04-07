6af112462a05cf7ba78fd27b21cbf189e7ad4833
ppambler <allinlbj@gmail.com>
2021-04-07 19:00:03 +0800

    美化登录页面和注册页面

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,89 @@
-import React,{useRef} from 'react'
-import {observer} from 'mobx-react'
-import {useStores} from '../stores'
+import React from "react";
+import styled from "styled-components"
+import { Form, Input, Button } from "antd";
 
-const Component = observer(() => {
-  console.log(useStores())
-  const {AuthStore} = useStores()
-  const inputRef = useRef()
-  console.log(AuthStore)
-  const bindChange = e => {
-    AuthStore.setUsername(inputRef.current.value)
-  }
+const Wrapper = styled.div`
+  max-width: 600px;
+  padding: 20px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 10px;
+  text-align: center;
+`;
+
+const layout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 20,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 4,
+    span: 20,
+  },
+};
+
+const Component = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <>
-      <h1>Login: {AuthStore.values.username}</h1>
-      <input onChange={bindChange} type="text" ref={inputRef}/>
-    </>
-  )
-})
+    <Wrapper>
+      <Title>登录</Title>
+      <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="用户名"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: '请输入你的用户名！',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-export default Component
+      <Form.Item
+        label="密码"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: '请输入你的密码！',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
+    </Wrapper>
+  );
+};
+
+export default Component;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,18 +1,97 @@
-import React, { useRef } from "react";
-import { observer } from "mobx-react";
-import { useStores } from "../stores";
+import React from "react";
+import styled from "styled-components";
+import { Form, Input, Button } from "antd";
 
-const Component = observer(() => {
-  const {AuthStore} = useStores()
-  const inputRef = useRef()
-  const bindChange = e => {
-    AuthStore.setUsername(inputRef.current.value)
-  }
+const Wrapper = styled.div`
+  max-width: 600px;
+  padding: 20px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 10px;
+  text-align: center;
+`;
+
+const layout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 20,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 4,
+    span: 20,
+  },
+};
+
+const Component = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <>
-      <h1>Register: {AuthStore.values.username}</h1>
-    </>
+    <Wrapper>
+      <Title>注册</Title>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的用户名！",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "请输入你的密码！",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="确认密码"
+          name="confirmPassword"
+          rules={[{ required: true, message: "请输入密码！" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
   );
-});
+};
 
 export default Component;
