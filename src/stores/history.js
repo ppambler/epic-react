a12b3227a449888d6c9e8063e82020a4bfc8fe8f
ppambler <allinlbj@gmail.com>
2021-04-13 18:48:34 +0800

    编写 HistoryStore 全局状态

---
+++ src/stores/history.js
@@ -0,0 +1,37 @@
+import { observable, action, makeObservable } from "mobx";
+import { Uploader } from "../models";
+import { message } from "antd";
+
+class HistoryStore {
+  constructor() {
+    makeObservable(this);
+  }
+  @observable list = [];
+  @observable isLoading = false;
+  @observable hasMore = true;
+  @observable page = 0;
+  limit = 10;
+
+  @action append(newList) {
+    this.list = this.list.concat(newList);
+  }
+
+  @action find() {
+    this.isLoading = true;
+    Uploader.find({ page: this.page, limit: this.limit })
+      .then((newList) => {
+        this.append(newList);
+        if (newList.length < this.limit) {
+          this.hasMore = false;
+        }
+      })
+      .catch((error) => {
+        message.error("加载数据失败", error);
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+}
+
+export default new HistoryStore();
